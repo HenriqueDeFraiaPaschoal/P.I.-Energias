deffbdee992dc565b9574b4a91a84d77e4001076
Achette <i.achette@live.com>
2022-11-15 17:31:17 -0300

    Chore: changed tags for better semantics

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,91 +1,92 @@
-const btnMobile = document.getElementById('btn-mobile');
+const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('nav');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
   if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
   } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
   }
 }
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 
 class SlideStories {
-    constructor(id) {
-      this.slide = document.querySelector(`[data-slide="${id}"]`);
-      this.active = 0;
-      this.init();
-    }
-  
-    activeSlide(index) {
-      this.active = index;
-      this.items.forEach((item) => item.classList.remove('active'));
-      this.items[index].classList.add('active');
-      this.thumbItems.forEach((item) => item.classList.remove('active'));
-      this.thumbItems[index].classList.add('active');
-      this.autoSlide();
-    }
-  
-    prev() {
-      if (this.active > 0) {
-        this.activeSlide(this.active - 1);
-      } else {
-        this.activeSlide(this.items.length - 1);
-      }
-    }
-  
-    next() {
-      if (this.active < this.items.length - 1) {
-        this.activeSlide(this.active + 1);
-      } else {
-        this.activeSlide(0);
-      }
-    }
-  
-    addNavigation() {
-      const nextBtn = this.slide.querySelector('.slide-next');
-      const prevBtn = this.slide.querySelector('.slide-prev');
-      nextBtn.addEventListener('click', this.next);
-      prevBtn.addEventListener('click', this.prev);
-    }
-  
-    addThumbItems() {
-      this.items.forEach(() => (this.thumb.innerHTML += `<span></span>`));
-      this.thumbItems = Array.from(this.thumb.children);
-    }
-  
-    autoSlide() {
-      clearTimeout(this.timeout);
-      this.timeout = setTimeout(this.next, 3000);
-    }
-  
-    init() {
-      this.next = this.next.bind(this);
-      this.prev = this.prev.bind(this);
-      this.items = this.slide.querySelectorAll('.slide-items > *');
-      this.thumb = this.slide.querySelector('.slide-thumb');
-      this.addThumbItems();
-      this.activeSlide(0);
-      this.addNavigation();
+  constructor(id) {
+    this.slide = document.querySelector(`[data-slide="${id}"]`);
+    this.active = 0;
+    this.init();
+  }
+
+  activeSlide(index) {
+    this.active = index;
+    this.items.forEach((item) => item.classList.remove("active"));
+    this.items[index].classList.add("active");
+    this.thumbItems.forEach((item) => item.classList.remove("active"));
+    this.thumbItems[index].classList.add("active");
+    this.autoSlide();
+  }
+
+  prev() {
+    if (this.active > 0) {
+      this.activeSlide(this.active - 1);
+    } else {
+      this.activeSlide(this.items.length - 1);
     }
   }
-  
-  new SlideStories('slide');
 
-  const button = document.querySelector('.btn-form')
-  const inputName = document.querySelector('#name')
-  const inputEmail = document.querySelector('#email')
-  const inputText = document.querySelector('#message')
+  next() {
+    if (this.active < this.items.length - 1) {
+      this.activeSlide(this.active + 1);
+    } else {
+      this.activeSlide(0);
+    }
+  }
 
-  button.addEventListener('click', () => {
-    alert('Formulário enviado com sucesso. Entraremos em contato em beve!')
-    inputName.value = ""
-    inputEmail.value = ""
-    inputText.value = ""
-  })
+  addNavigation() {
+    const nextBtn = this.slide.querySelector(".slide-next");
+    const prevBtn = this.slide.querySelector(".slide-prev");
+    nextBtn.addEventListener("click", this.next);
+    prevBtn.addEventListener("click", this.prev);
+  }
+
+  addThumbItems() {
+    this.items.forEach(() => (this.thumb.innerHTML += `<span></span>`));
+    this.thumbItems = Array.from(this.thumb.children);
+  }
+
+  autoSlide() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.next, 3000);
+  }
+
+  init() {
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.items = this.slide.querySelectorAll(".slide-items > *");
+    this.thumb = this.slide.querySelector(".slide-thumb");
+    this.addThumbItems();
+    this.activeSlide(0);
+    this.addNavigation();
+  }
+}
+
+new SlideStories("slide");
+
+const button = document.querySelector(".btn-form");
+const inputName = document.querySelector("#name");
+const inputEmail = document.querySelector("#email");
+const inputText = document.querySelector("#message");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  alert("Formulário enviado com sucesso. Entraremos em contato em beve!");
+  inputName.value = "";
+  inputEmail.value = "";
+  inputText.value = "";
+});
